04ccd0bae31e7c4cbfbaabd7ecc03965b2f70b8d
Шмырев Михаил Андреевич <pr119shmyrev@oat.local>
2022-11-02 10:47:46 +0600

    Добавьте файлы проекта.

---
+++ Praktika_31_10/AddUsersWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Praktika_31_10
+{
+    /// <summary>
+    /// Логика взаимодействия для AddUsersWindow.xaml
+    /// </summary>
+    public partial class AddUsersWindow : Window
+    {
+        public AddUsersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
